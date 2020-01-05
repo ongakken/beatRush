7d7946543f4ccf2067656ebfe5beca2d6d15a76c
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-01-05 23:21:10 +0100

    a first GPlay build;

--- beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
+++ beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-92414843L // the length of the file in bytes
+"2", // the version of the APK that the file was uploaded against
+90017179L // the length of the file in bytes
 )
 };
 };
