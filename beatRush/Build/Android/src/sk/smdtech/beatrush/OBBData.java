c9eb629d7e3f41e42e671e63d9bc324c46f1aa1a
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-01-06 00:21:08 +0100

    playable release, alpha;

--- beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
+++ beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"2", // the version of the APK that the file was uploaded against
-90017179L // the length of the file in bytes
+"3", // the version of the APK that the file was uploaded against
+90017192L // the length of the file in bytes
 )
 };
 };
--- beatRush/Plugins/EasyAds/Intermediate/Build/Win64/UE4Editor/Development/EasyAds/PCH.EasyAds.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/smdex/Documents/GitHub/beatRush/beatRush/Plugins/EasyAds/Intermediate/Build/Win64/UE4Editor/Development/EasyAds/PCH.EasyAds.h"
