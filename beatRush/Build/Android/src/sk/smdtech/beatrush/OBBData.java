ab4d408d9a244229adbdc9c33f5a61ca3673b051
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-01-05 22:25:22 +0100

    'ads not FUCKING ready';

--- beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
+++ beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-92412428L // the length of the file in bytes
+92414843L // the length of the file in bytes
 )
 };
 };
