ab1951e1112e0187e1651795bf092b7d8f37a09f
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-01-09 20:05:01 +0100

    a new build, 9;

--- beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
+++ beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"8", // the version of the APK that the file was uploaded against
-27622463L // the length of the file in bytes
+"9", // the version of the APK that the file was uploaded against
+27639200L // the length of the file in bytes
 )
 };
 };
