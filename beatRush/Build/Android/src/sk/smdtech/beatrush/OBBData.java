2607545176df7e4987873c2e17b00f3520efa3cb
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-01-12 14:06:01 +0100

    a new bugfix release;

--- beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
+++ beatRush/Build/Android/src/sk/smdtech/beatrush/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"10", // the version of the APK that the file was uploaded against
-27661599L // the length of the file in bytes
+"11", // the version of the APK that the file was uploaded against
+27654137L // the length of the file in bytes
 )
 };
 };
