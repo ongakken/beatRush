90c3baaf97889f32c57aed30c3cc78e38bb02e9f
Simon Slamka <51794014+simonSlamkaSMD@users.noreply.github.com>
2020-01-21 16:27:53 +0100

    rollback;

--- beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-armv7-es2.bat
+++ beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-armv7-es2.bat
@@ -6,11 +6,7 @@ if "%ANDROID_NDK_ROOT%"=="" set NDK_ROOT="C:\NVPACK\android-ndk-r14b"
 set NDKSTACK=%NDK_ROOT%
 dk-stack.cmd
 
-<<<<<<< HEAD
-%NDKSTACK% -sym beatRush_Symbols_v25/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
-=======
-%NDKSTACK% -sym beatRush_Symbols_v22/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
->>>>>>> parent of 7c0873e... fixed that bug with the menu xD;
+%NDKSTACK% -sym beatRush_Symbols_v2/beatRush-armv7-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
 
 goto:eof
 
