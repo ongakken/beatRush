4fe4a1b9d4bfb47331606d2857411c66a78833f7
Simon Slamka <51794014+simonSlamkaSMD@users.noreply.github.com>
2020-01-30 12:18:49 +0100

    submitted a new build; made the skybox rotate around all 3 axes every 0.01s;

--- beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-arm64-es2.bat
+++ beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-arm64-es2.bat
@@ -6,7 +6,7 @@ if "%ANDROID_NDK_ROOT%"=="" set NDK_ROOT="C:\NVPACK\android-ndk-r14b"
 set NDKSTACK=%NDK_ROOT%
 dk-stack.cmd
 
-%NDKSTACK% -sym beatRush_Symbols_v26/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
+%NDKSTACK% -sym beatRush_Symbols_v27/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
 
 goto:eof
 
