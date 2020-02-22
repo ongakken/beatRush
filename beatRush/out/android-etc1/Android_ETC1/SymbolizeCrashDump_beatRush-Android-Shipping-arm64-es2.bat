2a5ffbadc57fa231e7388897a350d47f98a3ba1a
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-02-22 18:57:37 +0100

    integrated IAP, but needs extensive testing;

--- beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-arm64-es2.bat
+++ beatRush/out/android-etc1/Android_ETC1/SymbolizeCrashDump_beatRush-Android-Shipping-arm64-es2.bat
@@ -6,7 +6,7 @@ if "%ANDROID_NDK_ROOT%"=="" set NDK_ROOT="C:\NVPACK\android-ndk-r14b"
 set NDKSTACK=%NDK_ROOT%
 dk-stack.cmd
 
-%NDKSTACK% -sym beatRush_Symbols_v36/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
+%NDKSTACK% -sym beatRush_Symbols_v37/beatRush-arm64-es2 -dump "%1" > beatRush_SymbolizedCallStackOutput.txt
 
 goto:eof
 
