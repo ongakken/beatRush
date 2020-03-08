72f9332f0beca7b8ff57f9ac3a692110cc3e44b5
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-03-08 12:06:44 +0100

    bugfixes; UI changes; logic bugfixes;

--- beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
+++ beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "sk.smdtech.beatrush";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 83;
-  public static final String VERSION_NAME = "0.90";
+  public static final int VERSION_CODE = 90;
+  public static final String VERSION_NAME = "0.97";
 }
