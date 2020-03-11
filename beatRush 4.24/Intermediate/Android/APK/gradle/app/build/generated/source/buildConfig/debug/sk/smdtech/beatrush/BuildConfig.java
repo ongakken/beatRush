91cc7ceb88a8ef3fefba7fd7eab7cb63bbbad285
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-03-11 18:44:33 +0100

    done with level 7; lowered app size by lowering sample rate of the OST waves by 50%;

--- beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
+++ beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "sk.smdtech.beatrush";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 94;
-  public static final String VERSION_NAME = "0.99";
+  public static final int VERSION_CODE = 97;
+  public static final String VERSION_NAME = "1.02";
 }
