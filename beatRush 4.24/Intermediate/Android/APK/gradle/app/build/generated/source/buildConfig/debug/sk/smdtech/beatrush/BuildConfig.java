5c2ef3ea01f0eee09483a6d51190d45c036f9327
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-03-16 00:19:15 +0100

    1.05; bugfixes; added a button; some other stuff I don't remember xD;

--- beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
+++ beatRush 4.24/Intermediate/Android/APK/gradle/app/build/generated/source/buildConfig/debug/sk/smdtech/beatrush/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "sk.smdtech.beatrush";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 98;
-  public static final String VERSION_NAME = "1.03";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.05";
 }
