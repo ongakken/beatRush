358c3ab2b12cdd53de5ba6e1664779a7cf108d59
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-05-14 22:38:13 +0200

    performance and bugfixes; two new releases;

---
+++ beatRush 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/ship.gen.cppa8.d
@@ -0,0 +1,6 @@
+D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/ship.gen.cppa8.o: \
+  D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/UE4/Inc/beatRush/ship.gen.cpp \
+  D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/beatRush/Shipping/Engine/SharedPCH.Engine.h \
+  D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/Definitions.beatRush.h \
+  D:/Epic\ Games/UE_4.24/Engine/Source/Runtime/CoreUObject/Public\UObject/GeneratedCppIncludes.h \
+  D:/Epic\ Games/UE_4.24/Engine/Source/Runtime/CoreUObject/Public\UObject/MetaData.h
