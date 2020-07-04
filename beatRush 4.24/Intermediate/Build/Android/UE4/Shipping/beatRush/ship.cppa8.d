edecbf9a44723be6d122a772517227464943d39e
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-07-04 23:25:27 +0200

    0.566; UI fixes; removed the old 4.24 Android BR version;

--- beatRush 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/ship.cppa8.d
+++
@@ -1,3 +0,0 @@
-D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/ship.cppa8.o: \
-  D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/beatRush/Shipping/Engine/SharedPCH.Engine.h \
-  D:/Unreal\ Projects/beatRush/beatRush\ 4.24/Intermediate/Build/Android/UE4/Shipping/beatRush/Definitions.beatRush.h
--- beatRush 4.24/Intermediate/Build/Win64/beatRushEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Unreal Projects/beatRush/beatRush 4.24/Intermediate/Build/Win64/beatRushEditor/Development/Engine/SharedPCH.Engine.h"
