d0f36e4967773f022c0ae2cbb7245127a0b7844d
simonSlamkaSMD <SMDexec000@protonmail.ch>
2020-09-10 17:22:24 +0200

    0.670; added Discord RCP - rich presence feature; attempted to add Steam Rich Presence too, but to no avail; got a Linux build as well ... it didn't compile, it seems ... so nothing xD;

--- beatRush_WIN/Source/beatRush/beatRush.Build.cs
+++ beatRush_WIN/Source/beatRush/beatRush.Build.cs
@@ -8,6 +8,6 @@ public class beatRush : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemUtils", "Steamworks" });
 	}
 }
